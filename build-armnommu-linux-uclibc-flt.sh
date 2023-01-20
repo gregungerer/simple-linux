70b3bfaf76746c6508315fde8fbe5658ade31281
Greg Ungerer <gerg@kernel.org>
2023-01-20 22:49:02 +1000

    build-armnommu: update binutils and gcc versions
    Update build to use the newest binutils (2.39) and newest gcc
    version (12.2).
    
    While we are updating tool versions also update the elf2flt version.
    Unfortunately elf2flt (any version) will need a patch to work with newer
    bfd libraries in modern tools (like binutils-2.39).
    
    Signed-off-by: Greg Ungerer <gerg@kernel.org>

--- build-armnommu-linux-uclibc-flt.sh
+++ build-armnommu-linux-uclibc-flt.sh
@@ -28,14 +28,14 @@ TARGET=arm-uclinuxeabi
 FLAVOR=armnommu-flt
 BOARD=versatile
 
-BINUTILS_VERSION=2.25.1
-GCC_VERSION=8.3.0
-ELF2FLT_VERSION=2019.12
+BINUTILS_VERSION=2.39
+GCC_VERSION=12.2.0
+ELF2FLT_VERSION=2021.08
 UCLIBC_VERSION=0.9.33.2
 LINUX_VERSION=6.0
 BUSYBOX_VERSION=1.35.0
 
-BINUTILS_URL=https://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.bz2
+BINUTILS_URL=https://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.xz
 GCC_URL=https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.xz
 UCLIBC_URL=https://www.uclibc.org/downloads/uClibc-${UCLIBC_VERSION}.tar.xz
 LINUX_URL=https://www.kernel.org/pub/linux/kernel/v6.x/linux-${LINUX_VERSION}.tar.xz
@@ -67,7 +67,7 @@ build_binutils()
 	echo "BUILD: building binutils-${BINUTILS_VERSION}"
 	fetch_file ${BINUTILS_URL}
 
-	tar xvjf downloads/binutils-${BINUTILS_VERSION}.tar.bz2
+	tar xvJf downloads/binutils-${BINUTILS_VERSION}.tar.xz
 	cd binutils-${BINUTILS_VERSION}
 	./configure --target=${TARGET} --prefix=${TOOLCHAIN}
 	make || exit 1
@@ -144,6 +144,9 @@ build_elf2flt()
 
 	tar xvzf downloads/v${ELF2FLT_VERSION}.tar.gz
 	cd elf2flt-${ELF2FLT_VERSION}
+
+	patch -p1 < ../patches/elf2flt-bfd-section-fixes.patch
+
 	./configure --with-libbfd=${ROOTDIR}/binutils-${BINUTILS_VERSION}/bfd/libbfd.a \
 		--with-libiberty=${ROOTDIR}/binutils-${BINUTILS_VERSION}/libiberty/libiberty.a \
 		--with-bfd-include-dir=${ROOTDIR}/binutils-${BINUTILS_VERSION}/bfd \
